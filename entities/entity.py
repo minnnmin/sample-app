16a4411dfac1499eba28975fbc0e1d4527d4f757
minnnmin <38430523+minnnmin@users.noreply.github.com>
2022-11-16 11:56:42 +0900

    Update entity.py

--- entities/entity.py
+++ entities/entity.py
@@ -3,11 +3,12 @@ from datetime import datetime
 from sqlalchemy import create_engine, Column, String, Integer, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-db_url = 'localhost:5432'
-db_name = 'mydb'
-db_user = 'postgres'
-db_password = 'test123'
+db_url = os.getenv("DB_URL")
+db_name = os.getenv("DB_NAME")
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PASSWORD")
 
 engine = create_engine(f'postgresql://{db_user}:{db_password}@{db_url}/{db_name}')
 Session = sessionmaker(bind=engine)
